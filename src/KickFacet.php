b8eb377a580792bca016eb55f9d04f888d758eac
Matthes <matthes@leuffen.de>
2021-10-22 11:46:48 +0200

    update

--- src/KickFacet.php
+++ src/KickFacet.php
@@ -93,13 +93,13 @@ class KickFacet
                     if (is_array($value))
                         continue;
 
-                    if (strpos($value, "\n") !== false) {
-                        // Multiline Strings: eval "varname=$'Quaoted\nString'" (Dollar is new bash syntax)
-                        // This is for backwards compatibility
-                        $value = "\$" . escapeshellarg($value);
-                        $value = str_replace("\r\n", '\n', $value);
-                        $value = str_replace("\n", '\n', $value);
-                    }
+
+                    // Multiline Strings: eval "varname=$'Quaoted\nString'" (Dollar is new bash syntax)
+                    // This is for backwards compatibility
+                    $value = "\$" . escapeshellarg($value);
+                    $value = str_replace("\r\n", '\n', $value);
+                    $value = str_replace("\n", '\n', $value);
+
                     $env = "export KICK_" . strtoupper($key) . "=$value";
                     echo "$env\n";
                 }
